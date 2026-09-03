34823c09bcf167e26c384f718f3eaa025d4ec14c
rafiadpr <work.rafiadrianp@gmail.com>
2026-09-03 15:03:33 +0700

    feat: add screenshot of flutter doctor & devices for week 1; code changes & screenshot on week 1 for week 2 warm-up; add skeleton for week 2

--- 02-week-2-declarative-ui-responsive-design/responsive_dashboard/lib/main.dart
+++ 02-week-2-declarative-ui-responsive-design/responsive_dashboard/lib/main.dart
@@ -1,93 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const ProfileApp());
+  runApp(const MyApp());
 }
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: Scaffold(
-//         appBar: AppBar(title: const Text('Profil Mahasiswa')),
-//         body: const Center(
-//           child: Column(mainAxisSize: MainAxisSize.min, children: [
-//             Icon(Icons.school, size: 72),
-//             SizedBox(height: 16),
-//             Text('Rafi Adrian Prasetya', style: TextStyle(fontSize: 24)),
-//             Text('NIM: 244107020026', style: TextStyle(fontSize: 18)),
-//             SizedBox(height: 8),
-//             Text('Pemrograman Mobile — Minggu 1'),
-//             Text('Teknologi Informasi, Politeknik Negeri Malang', style: TextStyle(fontSize: 16, color: Colors.grey)),
-//           ]),
-//         ),
-//       ),
-//     );
-//   }
-// }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-class ProfileApp extends StatelessWidget {
-  const ProfileApp({super.key});
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(child: ProfileCard()),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-    );
-  }
-}
-
-class ProfileCard extends StatelessWidget {
-  const ProfileCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 320,
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: Colors.indigo.shade50,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Row(
-            children: [
-              const CircleAvatar(child: Icon(Icons.person)),
-              const SizedBox(width: 12),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text('Nama Mahasiswa',
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-                    Text('...ketik nama Anda di sini...'),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 12),
-          const Row(children: [
-            Expanded(child: Text('NIM')),
-            Text('...ketik NIM Anda di sini...'),
-          ]),
-          const Row(children: [
-            Expanded(child: Text('Kelas')),
-            Text('...ketik kelas Anda di sini...'),
-          ]),
-          const Row(children: [
-            Expanded(child: Text('Email')),
-            Text('...ketik Email Anda di sini...'),
-          ]),
-        ],
-      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
